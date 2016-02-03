91a5eb9bfce049d225098e398a74fff68f0f4f13
Larry Adames <larry@leestapp.com>
2016-02-03 16:45:51 -0500

    Accounts are now linked to owners

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,7 +5,11 @@ feature 'Accounts' do
     visit root_path
     click_link "Create a new account"
     fill_in "Name", with: "Larry"
+    fill_in "Email", with: "larry@example.com"
+    fill_in "Password", with: "Password"
+    fill_in "Password confirmation", with: "Password"
     click_button "Create Account"
+    expect(page).to have_content("Signed in as larry@example.com")
     success_message = "Your account has been successfully created."
     expect(page).to have_content(success_message)
   end
