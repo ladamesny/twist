92f628c8800c9fe714a050d7749f3f5d919f1520
Larry Adames <larry@leestapp.com>
2016-02-03 17:11:37 -0500

    added subdomains to accounts

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -8,9 +8,11 @@ feature 'Accounts' do
     fill_in "Email", with: "larry@example.com"
     fill_in "Password", with: "Password"
     fill_in "Password confirmation", with: "Password"
+    fill_in "Subdomain", with: "test"
     click_button "Create Account"
     expect(page).to have_content("Signed in as larry@example.com")
     success_message = "Your account has been successfully created."
     expect(page).to have_content(success_message)
+    expect(page.current_url).to eq("http://test.lvh.me/")
   end
 end
