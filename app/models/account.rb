594affedaf0c6ad6c38318654ac3e332798d76a4
Larry Adames <larry@leestapp.com>
2016-02-04 18:37:27 -0500

    added uniqueness validation to account subdomain

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
+  validates :subdomain, presence: true, uniqueness: true
   accepts_nested_attributes_for :owner
 end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -15,4 +15,20 @@ feature 'Accounts' do
     expect(page).to have_content(success_message)
     expect(page.current_url).to eq("http://test.lvh.me/")
   end
+
+  scenario "Ensure subdomain uniqueness" do
+    Account.create(subdomain: "test", name: "Test")
+    visit root_path
+    click_link "Create a new account"
+    fill_in "Name", with: "Larry"
+    fill_in "Email", with: "larry@example.com"
+    fill_in "Password", with: "Password"
+    fill_in "Password confirmation", with: "Password"
+    fill_in "Subdomain", with: "test"
+    click_button "Create Account"
+    expect(page.current_url).to eq('http://lvh.me/accounts')
+    expect(page).to have_content("Sorry, your account could not be created.")
+    subdomain_text = find('.account_subdomain .help-block').text
+    expect(subdomain_text).to have_content("has already been taken")
+  end
 end
